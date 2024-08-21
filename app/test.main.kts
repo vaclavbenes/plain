d96db014970c524a901b60a61eba02222a761a08
Václav Beneš <vaclav.benes@gendigital.com>
2024-08-21 16:18:04 +0200

    Add kotlin script

---
+++ app/test.main.kts
@@ -0,0 +1,4 @@
+@file:Repository("https://artifactory.ida.avast.com/artifactory/maven-local")
+@file:Repository("https://artifactory.ida.avast.com/artifactory/repo1.maven.org-maven2")
+@file:DependsOn("com.avast.teamcity:avast-mobile-teamcity-kotlin-dsl-kubernetes:1.0.65")
+@file:DependsOn("com.google.code.gson:gson:2.10.1")
